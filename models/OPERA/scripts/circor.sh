9ea1ffd327660b04ebe4f03f8cdeb386727c9fb2
Carla Biermann <cb2318@cl.cam.ac.uk>
2025-03-12 11:30:21 +0000

    Scripts

--- models/OPERA/scripts/circor.sh
+++ models/OPERA/scripts/circor.sh
@@ -12,3 +12,8 @@ model_name="operaGT"
 dim=384
 python src/benchmark/processing/circor_processing.py --pretrain $model_name --dim $dim
 #python src/benchmark/linear_eval.py --task circor --pretrain $model_name --dim $dim --n_run 1
+
+
+python src/benchmark/processing/circor_processing.py --pretrain "vggish"
+python src/benchmark/processing/circor_processing.py --pretrain "clap"
+python src/benchmark/processing/circor_processing.py --pretrain "audiomae"
