a89ba6f79ffb4783ae3be6bd1a08bb5623e76462
Carla Biermann <cb2318@cl.cam.ac.uk>
2025-04-11 17:01:28 +0100

    allow linear head eval

--- models/OPERA/src/benchmark/other_eval/eval_finetuned_models.py
+++ models/OPERA/src/benchmark/other_eval/eval_finetuned_models.py
@@ -1,4 +1,3 @@
-import collections
 import numpy as np
 import os
 import glob
@@ -12,13 +11,124 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 from lightning.pytorch import seed_everything
 
-from src.model.models_eval import AudioClassifier, AudioClassifierAudioMAE, AudioClassifierCLAP
+from src.model.models_eval import (
+    AudioClassifier,
+    AudioClassifierAudioMAE,
+    AudioClassifierCLAP,
+    LinearHead,
+)
 from src.benchmark.model_util import get_encoder_path, initialize_pretrained_model
 from src.benchmark.other_eval.finetuning import AudioDataset, get_wandb_name
+from src.benchmark.linear_eval import FeatureDataset
 
 
-def evaluate_model(
+def evaluate_linear_head(
     seed,
+    metrics,
+    use_feature="operaCE1280",
+    l2_strength=1e-5,
+    epochs=64,
+    batch_size=32,
+    lr=1e-4,
+    head="linear",
+    dataset_name="circor",
+    task="murmurs",
+    feature_dir="feature/circor_eval/",
+    labels_filename="murmurs.npy",
+):
+    y_set = np.load(feature_dir + "train_test_split.npy")
+    y_label = np.load(feature_dir + labels_filename)
+    x_data = np.load(feature_dir + use_feature + "_feature.npy").squeeze()
+
+    feat_dim = x_data.shape[1]
+    n_cls = len(set(y_label))
+
+    x_data_test = x_data[y_set == "test"]
+    y_label_test = y_label[y_set == "test"]
+
+    test_data = FeatureDataset((x_data_test, y_label_test))
+
+    test_loader = DataLoader(
+        test_data, batch_size=batch_size, shuffle=True, num_workers=1
+    )
+
+    ck_path_dir = (
+        f"cks/linear/{dataset_name}_{task}/" if task else f"cks/linear/{dataset_name}"
+    )
+    ck_prefix = "_".join(
+        [
+            head,
+            use_feature,
+            str(batch_size),
+            str(lr),
+            str(epochs),
+            str(l2_strength),
+            str(seed),
+        ]
+    )
+
+    # Search for the checkpoint file(s) that match
+    ck_files = glob.glob(os.path.join(ck_path_dir, f"{ck_prefix}*.ckpt"))
+
+    # Pick the first match (or handle multiple matches if needed)
+    if not ck_files:
+        raise FileNotFoundError(
+            f"No checkpoint file found starting with {ck_prefix} in {ck_path_dir}"
+        )
+
+    ckpt_path = ck_files[0]
+    print(f"Found checkpoint: {ckpt_path}")
+
+    model = LinearHead.load_from_checkpoint(
+        checkpoint_path=ckpt_path,
+        feat_dim=feat_dim,
+        classes=n_cls,
+        l2_strength=l2_strength,
+        head=head,
+        metrics=metrics,
+        dataset=dataset_name,
+        task=task,
+    )
+
+    wandb_logger = WandbLogger(
+        project="Heart-Sound-Analysis",
+        name=get_wandb_name(use_feature, f"{dataset_name}-{task}", head),
+        log_model=False,
+    )
+
+    wandb_logger.experiment.config.update(
+        {
+            "n_cls": n_cls,
+            "use_feature": use_feature,
+            "l2_strength": l2_strength,
+            "epochs": epochs,
+            "batch_size": batch_size,
+            "lr": lr,
+            "head": head,
+            "dataset": dataset_name,
+            "task": task,
+            "seed": seed,
+            "gradient_clip_val": 1.0,
+        }
+    )
+
+    trainer = pl.Trainer(
+        accelerator="gpu" if torch.cuda.is_available() else "cpu",
+        devices=1,
+        logger=wandb_logger,
+    )
+
+    test_res = trainer.test(model=model, dataloaders=test_loader, ckpt_path=None)
+
+    auc = test_res[0]["test_auc"]
+
+    wandb.finish()
+    return auc
+
+
+def evaluate_finetuned_model(
+    seed,
+    metrics,
     pretrain="operaCE",
     l2_strength=1e-4,
     epochs=64,
@@ -33,25 +143,11 @@ def evaluate_model(
     freeze_encoder="none",  # Control freezing
 ):
     n_cls = len(set(np.load(feature_dir + labels_filename)))
-    metrics = [
-        "circor_weighted_murmur_acc",
-        "weighted_accuracy",
-        "weighted_auroc",
-        "weighted_specificity",
-        "weighted_recall",
-        "weighted_F1",
-        "unweighted_recall",
-        "avg_unweighted_recall",
-        "unweighted_precision",
-        "avg_unweighted_precision",
-        "unweighted_specificity",
-        "avg_unweighted_specificity",
-    ]
     ck_path_dir = (
         f"cks/finetune/{dataset_name}_{task}/"
         if task
         else f"cks/finetune/{dataset_name}"
-    ) 
+    )
     ck_prefix = "_".join(
         [
             "finetuning",
@@ -70,8 +166,10 @@ def evaluate_model(
 
     # Pick the first match (or handle multiple matches if needed)
     if not ck_files:
-        raise FileNotFoundError(f"No checkpoint file found starting with {ck_prefix} in {ck_path_dir}")
-        
+        raise FileNotFoundError(
+            f"No checkpoint file found starting with {ck_prefix} in {ck_path_dir}"
+        )
+
     ckpt_path = ck_files[0]
     print(f"Found checkpoint: {ckpt_path}")
 
@@ -230,39 +328,14 @@ def evaluate_model(
 
     y_set = np.load(feature_dir + "train_test_split.npy")
     y_label = np.load(feature_dir + labels_filename)
-    print(f"Label distribution: {collections.Counter(y_label)}")
-    print(f"Unique labels: {collections.Counter(y_set)}")
 
-    x_data_train = x_data[y_set == "train"]
-    y_label_train = y_label[y_set == "train"]
-    x_data_vad = x_data[y_set == "val"]
-    y_label_vad = y_label[y_set == "val"]
     x_data_test = x_data[y_set == "test"]
     y_label_test = y_label[y_set == "test"]
 
-    print(f"Train set label distributions {collections.Counter(y_label_train)}")
-    print(f"Val set label distributions {collections.Counter(y_label_vad)}")
-    print(f"Test set label distributions {collections.Counter(y_label_test)}")
-
-    train_data = AudioDataset(
-        (x_data_train, y_label_train),
-        augment=False,
-        max_len=False,
-        from_audio=from_audio,
-    )
     test_data = AudioDataset(
         (x_data_test, y_label_test), augment=False, max_len=False, from_audio=from_audio
     )
-    val_data = AudioDataset(
-        (x_data_vad, y_label_vad), augment=False, max_len=False, from_audio=from_audio
-    )
 
-    train_loader = DataLoader(
-        train_data, batch_size=batch_size, num_workers=2, shuffle=True
-    )
-    val_loader = DataLoader(
-        val_data, batch_size=batch_size, num_workers=2, shuffle=False
-    )
     test_loader = DataLoader(
         test_data, batch_size=batch_size, shuffle=False, num_workers=2
     )
@@ -273,93 +346,76 @@ def evaluate_model(
         logger=wandb_logger,
     )
 
-    trainer.test(model=model, dataloaders=train_loader, ckpt_path=None) # logging overwritten by test
-    trainer.test(model=model, dataloaders=val_loader, ckpt_path=None) # logging overwritten by test
     test_res = trainer.test(model=model, dataloaders=test_loader, ckpt_path=None)
-    
+
     auc = test_res[0]["test_auc"]
-    
+
     wandb.finish()
     return auc
 
 
-@hydra.main(config_path="../configs", config_name="finetune_config", version_base=None)
+def evaluate_model(cfg, seed):
+    eval_args = dict(
+        seed=seed,
+        metrics=cfg.metrics,
+        epochs=64,
+        l2_strength=cfg.l2_strength,
+    )
+
+    if cfg.task == "physionet16":
+        dataset_name = "physionet16"
+        task = ""
+        labels_filename = "labels.npy"
+        feature_dir = f"feature/{cfg.task}_eval/"
+    elif "zchsound" in cfg.task:
+        parts = cfg.task.split("_")
+        if "murmurs" in parts:  # new dataset / task notation for zchsound murmurs
+            dataset_name = f"{parts[0]}_{parts[1]}"
+            task = parts[2]
+        else:
+            dataset_name = "zchsound"
+            task = parts[1]
+        labels_filename = f"{task}.npy"
+        feature_dir = f"feature/{dataset_name}_eval/"
+    elif "circor" in cfg.task:
+        dataset_name = "circor"
+        task = cfg.task.split("_")[1]
+        labels_filename = f"{task}.npy"
+        feature_dir = "feature/circor_eval/"
+    elif "pascal" in cfg.task:
+        dataset_name = "pascal"
+        task = cfg.task.split("_")[1]
+        labels_filename = "labels.npy"
+        feature_dir = f"feature/{cfg.task}_eval/"
+    else:
+        raise ValueError(f"Unknown task: {cfg.task}")
+
+    eval_args.update(
+        dataset_name=dataset_name,
+        task=task,
+        labels_filename=labels_filename,
+        feature_dir=feature_dir,
+    )
+
+    if cfg.head_only:
+        return evaluate_linear_head(
+            use_feature=cfg.pretrain, head="linear", **eval_args
+        )
+    else:
+        return evaluate_finetuned_model(
+            pretrain=cfg.pretrain, head="linear", feat_dim=cfg.dim, **eval_args
+        )
+
+
+@hydra.main(config_path="../configs", config_name="eval_config", version_base=None)
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
 
     if not cfg.LOOCV:
         auc_scores = []
         for seed in range(cfg.n_run):
-            np.random.seed(seed)
-            torch.manual_seed(seed)
-            torch.cuda.manual_seed(seed)
             seed_everything(seed, workers=True)
-            if cfg.task == "circor_murmurs" or cfg.task == "circor_outcomes":
-                task = cfg.task.split("_")[1]
-                auc = evaluate_model(
-                    pretrain=cfg.pretrain,
-                    epochs=64,
-                    l2_strength=cfg.l2_strength,
-                    feat_dim=cfg.dim,
-                    dataset_name="circor",
-                    task=task,
-                    feature_dir="feature/circor_eval/",
-                    labels_filename=f"{task}.npy",
-                    seed=seed,
-                )
-            elif cfg.task == "zchsound_clean" or cfg.task == "zchsound_noisy": # ZCHSound outcomes
-                task = cfg.task.split("_")[1]
-                auc = evaluate_model(
-                    pretrain=cfg.pretrain,
-                    epochs=64,
-                    l2_strength=cfg.l2_strength,
-                    feat_dim=cfg.dim,
-                    dataset_name="zchsound",
-                    task=task,
-                    feature_dir=f"feature/{cfg.task}_eval/",
-                    labels_filename="labels.npy",
-                    seed=seed,
-                )
-            elif cfg.task == "zchsound_clean_murmurs" or cfg.task == "zchsound_noisy_murmurs": # ZCHSound murmurs
-                data_task_list = cfg.task.split("_")
-                dataset_name = f"{data_task_list[0]}_{data_task_list[1]}"
-                task = data_task_list[2]
-                auc = evaluate_model(
-                    pretrain=cfg.pretrain,
-                    epochs=64,
-                    l2_strength=cfg.l2_strength,
-                    feat_dim=cfg.dim,
-                    dataset_name=dataset_name,
-                    task=task,
-                    feature_dir=f"feature/{dataset_name}_eval/",
-                    labels_filename=f"{task}.npy",
-                    seed=seed,
-                )
-            elif cfg.task == "pascal_A" or cfg.task == "pascal_B":
-                task = cfg.task.split("_")[1]
-                auc = evaluate_model(
-                    pretrain=cfg.pretrain,
-                    epochs=64,
-                    l2_strength=cfg.l2_strength,
-                    feat_dim=cfg.dim,
-                    dataset_name="pascal",
-                    task=task,
-                    feature_dir=f"feature/{cfg.task}_eval/",
-                    labels_filename="labels.npy",
-                    seed=seed,
-                )
-            elif cfg.task == "physionet16":
-                auc = evaluate_model(
-                    pretrain=cfg.pretrain,
-                    epochs=64,
-                    l2_strength=cfg.l2_strength,
-                    feat_dim=cfg.dim,
-                    dataset_name="physionet16",
-                    task="",
-                    feature_dir=f"feature/{cfg.task}_eval/",
-                    labels_filename="labels.npy",
-                    seed=seed,
-                )
+            auc = evaluate_model(cfg, seed)
             auc_scores.append(auc)
         print("=" * 48)
         print(auc_scores)
